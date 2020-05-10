a0c00ac44877c9bc9e2389b4085fb09233eb99f3
paullawrence <Lawrence_pau@hotmail.co.uk>
2020-05-10 15:19:57 +0100

    Update basic data adding things

---
+++ Basic.BooksDb.Console/BasicBookData.cs
@@ -0,0 +1,28 @@
+﻿using BooksDb.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basic.BooksDb.Models
+{
+    public static class BasicBookData
+    {
+        public static IEnumerable<Book> Books() => new List<Book>
+        {
+            new Book("Book One", "Paul Lawrence", 1987, "This is the back of the book", DateTime.Parse("01/01/1987")),
+            new Book("Book Two", "Jenny Bookface", 1983, "What we say about a new tome", DateTime.Parse("03/06/1983")
+                        , new List<Review>
+                        {
+                            new Review("Written in spite", 8),
+                            new Review("Pre vengent tengent", 0),
+                            new Review("Caustic winds", 18)
+                        }),
+            new Book("A book in rhyme", "Floral Coral", 2007, "All up tin the beats", DateTime.Parse("14/05/2007")
+            , new List<Review>
+                        {
+                            new Review("Nice Weather for ducks", 3),
+                        })
+
+        };
+    }
+}
---
+++ Basic.BooksDb.Console/BookExtensions.cs
@@ -0,0 +1,33 @@
+﻿using BooksDb.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basic.BooksDb.Console
+{
+    public static class BookWithExtension
+    {
+        public static Book WithAuthor(this Book @this, string newAuthor)
+        {
+            return new Book(id: @this.Id, name: @this.Name, year: @this.Year, author: newAuthor,
+                            blurb: @this.Blurb, datePublished: @this.DatePublished, reviews: @this.Reviews
+                            );
+        }
+
+        public static Book WithDatePublished(this Book @this, DateTime newDatePublished)
+        {
+            return new Book(id: @this.Id, name: @this.Name, year: @this.Year, author: @this.Author,
+                            blurb: @this.Blurb, datePublished: newDatePublished, reviews: @this.Reviews
+                            );
+        }
+
+        public static Book WithReviews(this Book @this, IEnumerable<Review> newReviews)
+        {
+            return new Book(id: @this.Id, name: @this.Name, year: @this.Year, author: @this.Author,
+                            blurb: @this.Blurb, datePublished: @this.DatePublished, reviews: newReviews
+                            );
+        }
+    }
+
+
+}
